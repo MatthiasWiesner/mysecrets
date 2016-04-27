cbdbbd83f3ea83eac88c1a06544f55301a6ecc42
Matthias Wiesner <matthias.wiesner@googlemail.com>
2016-04-27 18:33:26 +0200

    Restructure css, js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,36 +5,35 @@ module.exports = function(grunt) {
         concat: {
             css: {
                src: [
-                 'static/css/bootstrap.css',
-                 'static/css/jquery-ui.css',
-                 'static/css/autocomplete.css',
-                 'static/css/main.css',
+                    'bower_components/bootstrap/dist/css/bootstrap.css',
+                    'bower_components/jquery-ui/themes/base/jquery-ui.css',
+                    'bower_components/jquery-ui/themes/base/autocomplete.css',
+                    'public/css/mysecrets.css',
                 ],
-                dest: 'static/css/combined.css'
+                dest: 'public/css/combined.css'
             },
             js : {
                 src : [
-                'static/js/dropbox-datastores-1.2-latest.js',
-                'static/js/jquery.js',
-                'static/js/jquery-ui.js',
-                'static/js/jquery.simulate.js',
-                'static/js/bootstrap.js',
-                'static/js/bootbox.js',
-                'static/js/jquery.storageapi.js',
-                'static/js/core.js',
-                'static/js/enc-base64.js',
-                'static/js/md5.js',
-                'static/js/evpkdf.js',
-                'static/js/cipher-core.js',
-                'static/js/aes.js',
-                'static/js/pbkdf2.js',
-                'static/js/mustache.js',
-                'static/js/clipboard.js',
-                'static/js/localbackend.js',
-                'static/js/dropboxbackend.js',
-                'static/js/main.js',
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/jquery-ui/jquery-ui.js',
+                    'bower_components/jquery-simulate/jquery.simulate.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'bower_components/bootbox/bootbox.js',
+                    'bower_components/jQuery-Storage-API/jquery.storageapi.js',
+                    'bower_components/cryptojslib/components/core.js',
+                    'bower_components/cryptojslib/components/enc-base64.js',
+                    'bower_components/cryptojslib/components/md5.js',
+                    'bower_components/cryptojslib/components/evpkdf.js',
+                    'bower_components/cryptojslib/components/cipher-core.js',
+                    'bower_components/cryptojslib/components/aes.js',
+                    'bower_components/cryptojslib/components/pbkdf2.js',
+                    'bower_components/mustache/mustache.js',
+                    'bower_components/clipboard/dist/clipboard.js',
+                    'bower_components/firebase/firebase.js',
+                    'public/js/localbackend.js',
+                    'public/js/mysecrets.js',
                 ],
-                dest : 'static/js/combined.js'
+                dest : 'public/js/combined.js'
             }
         },
         cssmin : {
@@ -42,14 +41,14 @@ module.exports = function(grunt) {
               rebase: false
             },
             css:{
-                src: 'static/css/combined.css',
-                dest: 'static/css/combined.min.css'
+                src: 'public/css/combined.css',
+                dest: 'public/css/combined.min.css'
             }
         },
         uglify : {
             js: {
                 files: {
-                    'static/js/combined.min.js' : [ 'static/js/combined.js' ]
+                    'public/js/combined.min.js' : [ 'public/js/combined.js' ]
                 }
             }
         },
